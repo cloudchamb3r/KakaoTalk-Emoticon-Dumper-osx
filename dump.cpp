3073de9cb5dd67d077c78a41e461d05eb6b4a7f6
cloudchamber <jizon0123@protonmail.com>
2023-03-31 04:31:14 +0900

    tiny refactoring...

--- dump.cpp
+++ dump.cpp
@@ -12,6 +12,11 @@
 #include <stdlib.h>
 #include <fstream>
 #include <sstream>
+#include <tuple>
+#include <memory>
+
+// global kernel return result
+kern_return_t kr;
 
 pid_t get_pid_from_name(const std::string& name) {
     size_t sz; 
@@ -29,54 +34,34 @@ pid_t get_pid_from_name(const std::string& name) {
     return 0;
 }
 
+std::tuple<vm_address_t, vm_size_t, vm_prot_t> get_region_info(vm_map_read_t task, vm_address_t address) {
+    vm_size_t size;
+    vm_region_submap_info_data_64_t info;
+    mach_msg_type_number_t cnt = VM_REGION_SUBMAP_INFO_COUNT_64;
+    natural_t depth = 30;
 
-std::string rwx(vm_prot_t prot) {
-    char status[] = {'-', '-', '-', 0};
-    if (prot & VM_PROT_READ) status[0] = 'r';
-    if (prot & VM_PROT_WRITE) status[1] = 'w';
-    if (prot & VM_PROT_EXECUTE) status[2] = 'x';
-    return status;
-}
-
-std::string share_mode(unsigned char sm) {
-    switch (sm)
-    {
-    case SM_COW:
-        return "CoW";
-    case SM_PRIVATE_ALIASED:
-        return "PrivateAliased";
-    case SM_PRIVATE:
-        return "Private";
-    case SM_EMPTY:
-        return "Empty";
-    case SM_SHARED_ALIASED:
-        return "SharedAliased";
-    case SM_SHARED:
-        return "Shared";
-    case SM_LARGE_PAGE:
-        return "LargePage";
-    case SM_TRUESHARED:
-        return "TrueShared";
-    default:
-        return "Unknown";
+    kr = vm_region_recurse_64(task, &address, &size, &depth, (vm_region_info_t)&info, &cnt);
+    if (kr != KERN_SUCCESS) {
+        return {0, 0, 0};
     }
+    return {address, size, info.protection};
 }
 
-int main(int argc, char** argv) {
+int main() {
+    // 1. check euid is root
     if (geteuid() != 0) {
-        fprintf(stderr, "this program need root priv.. x)\n");
+        fprintf(stderr, "This program need root priv.. x)\n");
         return 1;
     }
+
+    // 2. check kakaotalk process is existing
     pid_t kakaotalk_pid = get_pid_from_name("KakaoTalk");
     if (kakaotalk_pid == 0) {
         perror("Can't found Kakaotalk");
         return 1;
     }
-    printf("[+] kakaotalk pid : %d\n", kakaotalk_pid);
 
-
-    kern_return_t kr;
-
+    // 3. retreive kakaotalk task handle
     mach_port_t task;
     kr = task_for_pid(mach_task_self(), kakaotalk_pid, &task);
     if (kr != KERN_SUCCESS) {
@@ -84,55 +69,50 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    vm_size_t max_size = 0; 
-    vm_size_t size;
-    vm_region_submap_info_data_64_t info;
-    mach_port_t obj;
-    mach_msg_type_number_t cnt = VM_REGION_SUBMAP_INFO_COUNT_64;
-    natural_t depth = 30;
+    constexpr int riff_magic = 0x46464952;
+    constexpr int webp_magic = 0x50424557;
+    constexpr int mem_buffer_sz = 0x8000;
+    auto mem_buffer = std::make_unique<char[]>(mem_buffer_sz);
 
-    int riff_magic = 0x46464952;
-    int webp_magic = 0x50424557;
-    char * dump = new char[0x8000];
-
-    for (vm_address_t address = 0; true ; address += size) {
-        kr = vm_region_recurse_64(task, &address, &size, &depth, (vm_region_info_t)&info, &cnt);
-        if (kr != KERN_SUCCESS) {
-            fprintf(stderr, "[vm_region_recurse_64] failed : %s\n", mach_error_string(kr));
+    vm_address_t last_region_end = 0; 
+    while (true) {
+        auto [region_base, region_size, region_protection] = get_region_info(task, last_region_end);
+        if (region_base == 0) {
             break;
         }
-        if ((info.protection & VM_PROT_READ) && !(info.protection & VM_PROT_EXECUTE) && (info.protection & SM_PRIVATE)) {
-            size_t rdbytes = 0;
-            while (rdbytes < size) {
-                mach_vm_size_t _rdbytes;
-                kr = mach_vm_read_overwrite(task, address + rdbytes, 0x8000, (mach_vm_address_t)dump, &_rdbytes);
-                if (kr != KERN_SUCCESS) {
-                    break;
-                }
-
-                for (auto offset = 0ul ; offset + 8 < _rdbytes; offset += 4) {
-                    int cur = *((int*)(dump + offset));
-                    if (cur == riff_magic) {
-                        int next = *((int*)(dump + offset + 8));
-                        if (next == webp_magic) {
-                            int sz = *((int*)(dump + offset + 4)) + 12;
-                            if (sz <= 12) continue;
-
-                            char* webp_dump = new char[sz];
-                            mach_vm_size_t dummy;
-                            kr = mach_vm_read_overwrite(task, address + rdbytes + offset, sz, (mach_vm_address_t)webp_dump, &dummy);
-                            if (kr != KERN_SUCCESS) continue;
-                            
-                            // dump
-                            printf("[!] dumping..... on %012lx\n", address + rdbytes + offset);
-                            std::ostringstream filename; filename << std::hex << address + rdbytes + offset << ".webp"; 
-                            std::ofstream webp(filename.str(), std::ios_base::binary);
-                            webp.write(webp_dump, sz);
-                        }
+        printf("[+] inspecting %012lx-%012lx...\n", region_base, region_base + region_size);
+        if ((region_protection & VM_PROT_READ) && !(region_protection & VM_PROT_EXECUTE) && (region_protection & SM_PRIVATE)) {
+            size_t cursor = 0;
+            while (cursor < region_size) {
+                mach_vm_size_t rdbytes;
+                kr = mach_vm_read_overwrite(task, region_base + cursor, mem_buffer_sz, (mach_vm_address_t)mem_buffer.get(), &rdbytes);
+                if (kr != KERN_SUCCESS) break;
+                
+                // compare memory along 4byte align ;)
+                for (auto offset = 0u ; offset + 8 < rdbytes; offset += 4) {
+                    int _riff_magic = *((int*)(mem_buffer.get() + offset));
+                    int _size = *((int*)(mem_buffer.get() + offset + 4));
+                    int _webp_magic = *((int*)(mem_buffer.get() + offset + 8));
+             
+                    if (riff_magic == _riff_magic && webp_magic == _webp_magic && _size != 0) {         
+                        auto webp_dump_size = _size + 12;
+                        auto webp_dump = std::make_unique<char[]>(webp_dump_size);
+                        mach_vm_size_t _dummy;
+                        
+                        kr = mach_vm_read_overwrite(task, region_base + cursor + offset, webp_dump_size, (mach_vm_address_t)webp_dump.get(), &_dummy);
+                        if (kr != KERN_SUCCESS) continue;
+                        
+                        // dump
+                        printf("[!] dumping on %012lx\n", region_base + cursor + offset);
+                        std::ostringstream filename; filename << std::hex << region_base + cursor + offset << ".webp"; 
+                        std::ofstream webp(filename.str(), std::ios_base::binary);
+                        webp.write(webp_dump.get(), webp_dump_size);
                     }
                 }
-                rdbytes += _rdbytes;            
+
+                cursor += rdbytes;
             }
         }
+        last_region_end = region_base + region_size;
     }
 }
